f4be7516c6fca10f2c607a270f9129ac99a7a8d1
sabalearning01 <sabatemitayo44@gmail.com>
2024-07-31 12:33:53 +0100

    Newuser page

--- src/Pages/Newuser/Newuser.jsx
+++ src/Pages/Newuser/Newuser.jsx
@@ -1,30 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Newuser.css'
-import { useForm } from 'react-hook-form'
 
 
-const Newuser = () => {
-    const form = useForm();
 
-    const {register} = form;
-    
+    const Newuser = () => {
+  
+    const [formdata, setFormdata] = useState({
+      email:'',
+      password:'',
+    })
 
+
+    const [errors, setError] = useState({
+
+    })
+
+       function handleChange(event){
+        const { name, value} = event.target;
+        console.log(handleChange)
+        setFormdata({
+          ...formdata, [name] : value
+        })
+       }
+
+      const handleSubmit = (event)=>{
+        event.preventDefault()
+        const validationErrors ={}
+        if(!formdata.email.trim()){
+          validationErrors.email = "email address is required"
+        }else if (/\S+@\S+\.\S+/.test(formdata.email)){
+            validationErrors.email = "email address is not valid"
+        }
+
+
+        if(!formdata.password.trim()){
+          validationErrors.password = "password is required"
+        }else if (formdata.password.length < 6){
+            validationErrors.password = "password should be at least 6 characters"
+        }
+
+        setError(validationErrors)
+
+        if (Object.keys(validationErrors). length===0){
+          alert("Form Submitted Successfully")
+        }
+      }
 
   return (
     <div className='App'>
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit}>
        <h1 className='log'>Log in</h1>
 
-     <div className='form-control'>
+     <div className='form-control1'>
       <label htmlFor='Email'>Email Address</label>
-      <input type="email" id="email" placeholder='Your email address here' {...register(email)} />
+      <input type="email" id="email" placeholder='Your email address here' onChange={handleChange} />
+      {errors.email && <span>{errors.email}</span>}
       </div>
 
 
-      <div className='form-control'>
+      <div className='form-control2'>
       <label htmlFor='Password'>Password</label>
-      <input type="password" id="password" placeholder='*****' {...register(Password)} />
+      <input type="password" id="password" placeholder='*****' onChange={handleChange}  />
       <p className='fgt'>Forgot password?</p>
+      {errors.password && <span>{errors.password}</span>}
       </div>
 
 
--- src/Pages/Resetpassword/Resetpassword.jsx
+++ src/Pages/Resetpassword/Resetpassword.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 import './Resetpassword.css'
 
 const Resetpassword = () => {
+  // const [formdata, setFormdata] = useState({
+  //   email:'',
+  //   password:'',
+  // })
+
+
+  //    function handleChange(event){
+  //     const { name, value} = event.target;
+  //     setFormdata({
+  //       ...formdata, [name] : value
+  //     })
+  //   }
+
+  //   const handleSubmit = (event)=>{
+  //     event.preventDefault()
+  //     const validationErrors ={}
+  //     if(!formdata.email.trim()){
+  //       validationErrors.email = "email address is required"
+  //     }else if (/\S+@\S\.S+/.test(formdata.email)){
+  //         validationErrors.email = "email address is not valid"
+  //     }
+
+
+  //     if(!formdata.password.trim()){
+  //       validationErrors.password = "password is required"
+  //     }else if (formdata.password.length < 6){
+  //         validationErrors.password = "password should be at least 6 characters"
+  //     }
+
   return (
     <div className='newpassword'>
     <form>
@@ -28,4 +57,5 @@ const Resetpassword = () => {
   )
 }
 
+// }
 export default Resetpassword
