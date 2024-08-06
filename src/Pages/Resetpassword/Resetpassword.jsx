c12ed1891b3af426a3e6ae7236a55c464f94c630
sabalearning01 <sabatemitayo44@gmail.com>
2024-08-06 08:30:15 +0100

    resetpassword

--- src/Pages/Resetpassword/Resetpassword.jsx
+++ src/Pages/Resetpassword/Resetpassword.jsx
@@ -2,51 +2,73 @@ import React from 'react'
 import './Resetpassword.css'
 
 const Resetpassword = () => {
-  // const [formdata, setFormdata] = useState({
-  //   email:'',
-  //   password:'',
-  // })
+
+    const [oldPassword, setoldPassword] = useState('')
+    const [newPassword, setNewPassword] = useState('')
+    const [message,setMessage] = useState('');
+              
+    const validatePassword = () => {
+    if (oldPassword === newPassword){
+      setMessage('The new password can not be the same with the old password.');
+      return;
+    }
+
+    if (newPassword.length < 8){
+      setMessage('The new password must be at least 8 characters long ');
+      return;
+    }
+
+    if(!/\d/.test(newPassword)){
+           setMessage('The new password must contain at least one digit');
+           return;
+    }
 
 
-  //    function handleChange(event){
-  //     const { name, value} = event.target;
-  //     setFormdata({
-  //       ...formdata, [name] : value
-  //     })
-  //   }
-
-  //   const handleSubmit = (event)=>{
-  //     event.preventDefault()
-  //     const validationErrors ={}
-  //     if(!formdata.email.trim()){
-  //       validationErrors.email = "email address is required"
-  //     }else if (/\S+@\S\.S+/.test(formdata.email)){
-  //         validationErrors.email = "email address is not valid"
-  //     }
+    if (!/[A-Z]/.test(newPassword)) {
+      setMessage('The new password must contain at least one uppercase letter.');
+      return;
+    }
 
 
-  //     if(!formdata.password.trim()){
-  //       validationErrors.password = "password is required"
-  //     }else if (formdata.password.length < 6){
-  //         validationErrors.password = "password should be at least 6 characters"
-  //     }
+    if (!/[a-z]/.test(newPassword)) {
+      setMessage('The new password must contain at least one lowercase letter.');
+      return;
+    }
+    
+    if(!/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/.test(newPassword)){
+       setMessage('The new password must contain at least one special character.');
+       return;
+    }
+
+      setMessage('The new password is valid');
+    }
+
+    const handleSubmit = (event) => {
+            event.preventDefault();
+            validatePassword();
+    };
+
 
   return (
     <div className='newpassword'>
-    <form>
-       <div className='form-controll1'>
+    <form onSubmit = {handleSubmit}>
+       <div className='form-controll'>
    <h1 className='reset'>Reset Password?</h1>
    <p className='you'>You are  about to get a new password</p>
    </div> 
 
    <div className='form-controll'>
    <label>NewPassword</label>
-   <input type="password" id="password" placeholder='*********' />
+   <input type="password"
+    id="password"
+    placeholder='*********'
+    onChange={(event)=> setNewPassword(event.target.value)} />
    </div>
 
    <div className='form-controll'>
    <label>New Confirm Password</label>
-   <input type="password" id="password"   placeholder='*********' />
+   <input type="password" id="password"   placeholder='*********'
+   onChange = {(event) => setNewPassword(event.target.value) } />
    </div>
 
    <button className='resetpassword' type="submit">Reset Password</button>
